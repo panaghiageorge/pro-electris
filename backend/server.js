a21da51d2acf4af731a822854fae39e44599bfb9
Panaghia George <panaghiageorge@yahoo.com>
2021-04-24 14:51:51 +0300

    models

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -13,5 +17,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
