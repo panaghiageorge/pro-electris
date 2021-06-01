3eb5fec9fe1a059378557bfbf4d15ac827613813
Panaghia George <panaghiageorge@yahoo.com>
2021-06-01 13:21:39 +0300

    add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Container>
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/' component={HomeScreen} exact />
-          {/* <HomeScreen /> */}
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
